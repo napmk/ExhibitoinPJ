2908a01fa7d3960f0835497cccaac7626d082881
napmk <napmkmk@naver.com>
2023-01-18 16:55:16 +0900

    서버 올리는법

--- src/main/java/com/exhibition/ExhibitionApplication.java
+++ src/main/java/com/exhibition/ExhibitionApplication.java
@@ -1,13 +1,27 @@
 package com.exhibition;
 
+
+
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExhibitionApplication {
+public class ExhibitionApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExhibitionApplication.class, args);
-	}
+   public static void main(String[] args) {
+      SpringApplication.run(ExhibitionApplication.class, args);
+   }
 
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      // TODO Auto-generated method stub
+      return builder.sources(ExhibitionApplication.class);
+   }
+
+   
+   
 }
